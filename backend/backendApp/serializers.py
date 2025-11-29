39f0b8cc2e3eeb3e99a7df8d0b18c09e9d2ba52a
Daniel <daniel.gorniak.stud@pw.edu.pl>
2025-11-29 19:40:14 +0100

    integrate analysis to backedn

--- backend/backendApp/serializers.py
+++ backend/backendApp/serializers.py
@@ -1,9 +1,13 @@
 
 from rest_framework import serializers
-from .models import Email
+from .models import Email, LLMAnalysis
 class EmailSerializerGet(serializers.ModelSerializer):  # type: ignore[misc]
     class Meta:
         model = Email
         fields = ["sender_name", "sender_email", "recipient_name", 
                   "recipient_email", "subject", "date", "message_content"]
-        
+
+class LLMAnalysisSerializerGet(serializers.ModelSerializer):  # type: ignore[misc]
+    class Meta:
+        model = LLMAnalysis
+        fields = ["question", "answer"]
